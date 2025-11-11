77c7a688e214763bd173e0f08a239dcf1edf25f9
happyzqy <709751645@qq.com>
2025-11-11 19:43:32 +0800

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,5 @@ cd /disk/zdata1/home/zhangqingyu/work_guo/gui/VLMEvalKit
 # python run.py --data ScreenSpot --model Qwen3-VL-2B-Instruct --verbose
 python run.py --data AITZ_LOCAL --model Qwen3-VL-2B-Instruct --verbose
 
-
+#123
 #torchrun --nproc-per-node=8 run.py --data MMBench_DEV_EN MME SEEDBench_IMG --model idefics_80b_instruct qwen_chat mPLUG-Owl2 --verbose
